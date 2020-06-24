e7ae9ab0bce4e2dbe1509e7768fefedd7cd4e1f2
Tobin C. Harding <tobin@coblox.tech>
2020-06-24 13:36:52 +1000

    Re-order lib
    No code changes except moving functions around.

--- src/lib.rs
+++ src/lib.rs
@@ -22,51 +22,6 @@ use std::{
     time::Duration,
 };
 
-pub type PingPongTransport = Boxed<
-    (PeerId, StreamMuxerBox),
-    TransportTimeoutError<
-        EitherError<
-            EitherError<DnsErr<io::Error>, UpgradeError<SecioError>>,
-            UpgradeError<EitherError<io::Error, io::Error>>,
-        >,
-    >,
->;
-
-/// Builds a libp2p transport with the following features:
-/// - TcpConnection
-/// - DNS name resolution
-/// - authentication via secio
-/// - multiplexing via yamux or mplex
-pub fn build_transport(keypair: identity::Keypair) -> anyhow::Result<PingPongTransport> {
-    let transport = TcpConfig::new().nodelay(true);
-    let transport = DnsConfig::new(transport)?;
-
-    let transport = transport
-        .upgrade(Version::V1)
-        .authenticate(SecioConfig::new(keypair))
-        .multiplex(SelectUpgrade::new(
-            yamux::Config::default(),
-            MplexConfig::new(),
-        ))
-        .map(|(peer, muxer), _| (peer, StreamMuxerBox::new(muxer)))
-        .timeout(Duration::from_secs(20))
-        .boxed();
-
-    Ok(transport)
-}
-
-pub fn build_swarm(config: PingConfig) -> Result<Swarm<Ping>> {
-    let id_keys = identity::Keypair::generate_ed25519();
-    let peer_id = PeerId::from(id_keys.public());
-
-    let transport = crate::build_transport(id_keys)?;
-    let behaviour = Ping::new(config);
-
-    let swarm = Swarm::new(transport, behaviour, peer_id);
-
-    Ok(swarm)
-}
-
 /// Entry point to run the ping-pong app as a dialer.
 pub async fn run_dialer(addr: Multiaddr) -> Result<()> {
     let config = PingConfig::new()
@@ -115,3 +70,48 @@ pub async fn run_listener(addr: Multiaddr) -> Result<()> {
 
     Ok(())
 }
+
+pub fn build_swarm(config: PingConfig) -> Result<Swarm<Ping>> {
+    let id_keys = identity::Keypair::generate_ed25519();
+    let peer_id = PeerId::from(id_keys.public());
+
+    let transport = crate::build_transport(id_keys)?;
+    let behaviour = Ping::new(config);
+
+    let swarm = Swarm::new(transport, behaviour, peer_id);
+
+    Ok(swarm)
+}
+
+/// Builds a libp2p transport with the following features:
+/// - TcpConnection
+/// - DNS name resolution
+/// - authentication via secio
+/// - multiplexing via yamux or mplex
+pub fn build_transport(keypair: identity::Keypair) -> anyhow::Result<PingPongTransport> {
+    let transport = TcpConfig::new().nodelay(true);
+    let transport = DnsConfig::new(transport)?;
+
+    let transport = transport
+        .upgrade(Version::V1)
+        .authenticate(SecioConfig::new(keypair))
+        .multiplex(SelectUpgrade::new(
+            yamux::Config::default(),
+            MplexConfig::new(),
+        ))
+        .map(|(peer, muxer), _| (peer, StreamMuxerBox::new(muxer)))
+        .timeout(Duration::from_secs(20))
+        .boxed();
+
+    Ok(transport)
+}
+
+pub type PingPongTransport = Boxed<
+    (PeerId, StreamMuxerBox),
+    TransportTimeoutError<
+        EitherError<
+            EitherError<DnsErr<io::Error>, UpgradeError<SecioError>>,
+            UpgradeError<EitherError<io::Error, io::Error>>,
+        >,
+    >,
+>;
